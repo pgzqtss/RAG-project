78cd1bb46b768dc3045cbdfed0ba30e4c8b95205
pgzqtss <znkjennifer58@gmail.com>
2024-11-28 13:04:54 +0000

    Add changes to langChain_pinecone.py

---
+++ langChain_pinecone.py
@@ -0,0 +1,46 @@
+from langchain_community.document_loaders import PyPDFLoader, OnlinePDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+file_path = "/mnt/c/Users/znkje/OneDrive/Desktop/Systems/example.pdf"
+
+loader = PyPDFLoader(file_path=file_path)
+
+data = loader.load()
+
+# PyPDFLoader splits the document into pages
+print(f'You have {len(data)} document(s) /pages in your data')
+print(f'There are {len(data[0].page_content)} characters in your sample document')
+# print(f'Here is a sample: {data[0].page_content[:500]}')
+
+# Split the text into sentences (chunk size = 1000 characters)
+# We need chunk overlap to maintain context between chunks for NLP
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+
+print (f'Now you have {len(texts)} documents')
+
+# Create Embeddings of the text
+from langchain_community.vectorstores import Pinecone as LangchainPinecone
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+import os
+
+embeddings = OpenAIEmbeddings(openai_api_key="openapikey")
+# initialize pinecone
+pc = Pinecone(
+    api_key = "pineconeapikey",
+)
+
+if 'my-index' not in pc.list_indexes().names():
+    pc.create_index(
+        name='my-index',
+        dimension=1536,
+        metric='cosine',
+        spec=ServerlessSpec(
+            cloud='aws',
+            ragion='us-east-1',
+        )
+    )
+index_name = "my-index"
+
+docsearch = LangchainPinecone.from_texts([t.page_content for t in texts], embeddings, index_name = index_name)

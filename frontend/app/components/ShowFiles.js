eee097223a76bad9d7fd17d297a8e590c82a8ddf
winghoy <winghoyeung9@gmail.com>
2025-03-06 00:12:41 +0000

    Scrollable now if you show more than 10 files

--- frontend/app/components/ShowFiles.js
+++ frontend/app/components/ShowFiles.js
@@ -14,7 +14,7 @@ export default function ShowFiles({ id }) {
     <div className='flex flex-col w-full'>
       <div className='flex w-full justify-start'>
         <button 
-          className='text-sm w-[120px] p-2 mb-4 rounded-xl bg-gray-100 hover:bg-gray-200 transition hover:scale-105'
+          className='text-sm w-[120px] p-2 mb-2 rounded-xl bg-gray-100 hover:bg-gray-200 transition hover:scale-105'
           onClick={() => {setShowFiles(!showFiles); setRefresh(!refresh); }}
         >
           <div className='flex justify-center gap-x-2'>
@@ -23,14 +23,14 @@ export default function ShowFiles({ id }) {
           </div>
         </button>
       </div>
-      <div>
+      <div className={`${showFiles ? 'bg-gray-50 p-2 rounded-2xl mb-2' : ''}`}>
         {showFiles && files != undefined && files.length > 0 && (
-          <ul className='grid grid-cols-6 gap-2 mb-4'>
+          <ul className='grid grid-cols-6 gap-2 max-h-[90px] overflow-auto'>
             {files.map((file, index) => (
               <li key={index} className='col-span-1'>
                 <a href={`files/${id}/${file}`} target='_blank' rel='noopener noreferrer'>
                   <div
-                    className='flex items-center justify-between w-full bg-gray-100 border-[1px] border-gray-200 rounded-xl py-1 px-2 hover:bg-gray-200'
+                    className='flex items-center justify-between w-full bg-gray-100 border-[1px] border-gray-200 rounded-xl py-1 px-2 hover:bg-gray-200 mb-2'
                   >
                     <span className='text-sm overflow-hidden whitespace-nowrap text-ellipsis flex-1'>
                       {file}
